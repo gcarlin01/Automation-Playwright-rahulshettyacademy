6ab7a3c58aacd0c90c270bd063d02a94d83c3583
gcarlin01 <giancarlo.carlin01@gmail.com>
2024-02-26 20:22:52 -0500

    adds indentation to settings

--- tests/clientAppOrderFlow.spec.ts
+++ tests/clientAppOrderFlow.spec.ts
@@ -1,12 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test.describe("Client App Order Flow", () => {
-    // test.use({ storageState: "LoggedInState.json" });
-    test("User adds products to cart", async ({ page }) => {
-     await page.goto("https://rahulshettyacademy.com/client");   
-     await page.locator(".card-body b").first().waitFor();
-     const titles = await page.locator(".card-body b").allTextContents();
-     console.log(titles); 
-    })
-}
-);
+  test("User adds products to cart", async ({ page }) => {
+    await page.goto("https://rahulshettyacademy.com/client");   
+    await page.locator(".card-body b").first().waitFor();
+    const titles = await page.locator(".card-body b").allTextContents();
+    console.log(titles); 
+  })
+});
