fe908eb2ad1cee9bbae238c72fce3a77a189325e
Giancarlo Carlin <giancarlo.carlin01@gmail.com>
2024-02-26 19:41:46 -0500

    Update clientAppOrderFlow.spec.ts

--- tests/clientAppOrderFlow.spec.ts
+++ tests/clientAppOrderFlow.spec.ts
@@ -7,15 +7,6 @@ test.describe("Client App Order Flow", () => {
     await page.locator(".card-body b").first().waitFor();
    const titles = await page.locator(".card-body b").allTextContents();
    console.log(titles); 
-//    const count = await products.count();
-//    for (let i = 0; i < count; ++i) {
-//       if (await products.nth(i).locator("b").textContent() === productName) {
-//          //add to cart
-//          await products.nth(i).locator("text= Add To Cart").click();
-//          break;
     })
 }
 );
-
-//    await page.locator("[routerlink*='cart']").click();
-   //await page.pause();
